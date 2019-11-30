6c7ecdfe4539dcfd774071521fdf412d62246a2d
Juliano Sirtori <julianosirtori@gmail.com>
2019-11-30 14:46:17 -0300

    Finished

---
+++ README.md
@@ -0,0 +1,17 @@
+# RocketFy
+RocketFy is a clone the <a hfre="https://www.pipefy.com/pt-br/">pipefy'</a> interface
+
+Tutorial Video: https://www.youtube.com/watch?v=awRtgpRsdTQ
+
+Original Repository : https://github.com/Rocketseat/youtube-ui-clone-pipefy
+
+PrintScreen:
+![](images/printscreen.png)
+
+
+## Installation and Development setup
+FrontEnd:
+```sh
+yarn install
+yarn start
+```
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,53 @@
-import React from 'react';
-import { useDrag } from 'react-dnd';
+import React, {useRef, useContext} from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+
+import BoardContext from '../Board/context';
 
 import { Container, Label } from './styles';
 
-export default function Card({ data }) {
+export default function Card({ data, index, listIndex }) {
+  const ref = useRef();
+  const { move } = useContext(BoardContext);
+
   const [{ isDragging }, dragRef] =  useDrag({
-    item: { type: 'CARD' },
+    item: { type: 'CARD', index, content: data.content, id: data.id, listIndex },
     collect: monitor => ({
       isDragging: monitor.isDragging()
     })
   })
 
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover(item, monitor){
+        const draggedListIndex = item.listIndex;
+        const targetListIndex = listIndex;
 
+        const draggedIndex = item.index;
+        const targetIndex = index;
+
+        if(draggedIndex === targetIndex && draggedListIndex === targetListIndex ) return;
+        
+        const targetSize = ref.current.getBoundingClientRect();
+        const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+        const draggedOffset = monitor.getClientOffset();
+        const draggedTop = draggedOffset.y - targetSize.top;
+
+        if(draggedIndex < targetIndex && draggedTop < targetCenter && draggedListIndex === targetListIndex )  return
+        if(draggedIndex > targetIndex && draggedTop > targetCenter && draggedListIndex === targetListIndex )  return
+
+        move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+        
+        item.index = targetIndex;
+        item.listIndex = targetListIndex;
+
+    }
+  })
+
+  dragRef(dropRef(ref));
 
   return (
-    <Container isDragging={isDragging} ref={dragRef}>
+    <Container isDragging={isDragging} ref={ref}>
       <header>
         {data.labels.map(label => <Label key={label} color={label} />)}
       </header>
